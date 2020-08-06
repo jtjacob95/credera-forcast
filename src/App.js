d19a21ddc6d1fcceea772337dd3c39fd432f3e48
Joel Jacob <jtjacob95@gmail.com>
2020-08-06 01:10:15 -0500

    Core HTML frame created

--- src/App.js
+++ src/App.js
@@ -2,23 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function DayWeather(props){
+  return(
+    <div className="day-box">
+      <div className="day-of-week">
+        {props.day}
+      </div>
+      <img src={props.logo} className="day-weather-icon"></img>
+      <div className="day-temperature">
+        {props.temperature}
+      </div>
+    </div>
+  );
+}
+
+function MainWeather(props){
+  return(
+    <div className="day-box">
+      <div className="day-of-week">
+        {props.day}
+      </div>
+      <img src={props.logo} className="day-weather-icon"></img>
+      <div className="day-temperature">
+        {props.temperature}
+      </div>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className = "title-date-loc"> 
+          Dallas, Texas
+
+        </div>
+        <div>
+          <MainWeather day="saturday" logo={logo} temperature="80"/>
+          <div>
+            <DayWeather day="sunday" logo={logo} temperature="70"/>
+            <DayWeather day="monday" logo={logo} temperature="72"/>
+            <DayWeather day="tuesday" logo={logo} temperature="74"/>
+            <DayWeather day="wednesday" logo={logo} temperature="76"/>
+            <DayWeather day="thursday" logo={logo} temperature="78"/>
+          </div>
+          
+        </div>
     </div>
   );
 }
