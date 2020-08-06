6389ff33901e056615e5ca2cd258a879e2a2d5b4
Joel Jacob <jtjacob95@gmail.com>
2020-08-06 03:21:27 -0500

    Linked HTML to API

--- src/App.js
+++ src/App.js
@@ -31,26 +31,56 @@ function MainWeather(props){
 }
 
 
-function App() {
-  return (
-    <div className="App">
-        <div className = "title-date-loc"> 
-          Dallas, Texas
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    var d = new Date();
 
-        </div>
-        <div>
-          <MainWeather day="saturday" logo={logo} temperature="80"/>
-          <div>
-            <DayWeather day="sunday" logo={logo} temperature="70"/>
-            <DayWeather day="monday" logo={logo} temperature="72"/>
-            <DayWeather day="tuesday" logo={logo} temperature="74"/>
-            <DayWeather day="wednesday" logo={logo} temperature="76"/>
-            <DayWeather day="thursday" logo={logo} temperature="78"/>
+    this.state = {
+        dat : [],
+        currDay : d.getDay()
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=32.783058&lon=-96.806671&exclude=current,minutely,hourly&appid=7eaf51d1f84ee9ba13b31105252aab59")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            success: true,
+            dat: result.daily,
+          });
+          console.log(this.state.dat);
+        },
+        (error) => {
+          this.setState({
+            success: false,
+          });
+        }
+      )
+  }
+ 
+  render(){
+    return (
+      <div className="App">
+          <div className = "title-date-loc"> 
+            Dallas, Texas
           </div>
-          
-        </div>
-    </div>
-  );
+          <div>
+            <MainWeather day="saturday" logo={logo} temperature="80"/>
+            <div>
+              <DayWeather day="sunday" logo={logo} temperature="70"/>
+              <DayWeather day="monday" logo={logo} temperature="72"/>
+              <DayWeather day="tuesday" logo={logo} temperature="74"/>
+              <DayWeather day="wednesday" logo={logo} temperature="76"/>
+              <DayWeather day="thursday" logo={logo} temperature="78"/>
+            </div>
+            
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
