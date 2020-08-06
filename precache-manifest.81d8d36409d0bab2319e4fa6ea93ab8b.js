dd411d12f858c221d6ac653e15bf6a343b14abd8
Joel Jacob <jtjacob95@gmail.com>
2020-08-06 16:34:33 -0500

    Updates

---
+++ precache-manifest.81d8d36409d0bab2319e4fa6ea93ab8b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3226004ffdda4bc2686d92731472aebc",
+    "url": "/credera-forcast/index.html"
+  },
+  {
+    "revision": "797bbe3b07512c974ee2",
+    "url": "/credera-forcast/static/css/main.28884b46.chunk.css"
+  },
+  {
+    "revision": "cccabc864b6d248fc11c",
+    "url": "/credera-forcast/static/js/2.47a5ff91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/credera-forcast/static/js/2.47a5ff91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797bbe3b07512c974ee2",
+    "url": "/credera-forcast/static/js/main.e30ef9c5.chunk.js"
+  },
+  {
+    "revision": "de623ad9a2240a27b107",
+    "url": "/credera-forcast/static/js/runtime-main.2358396d.js"
+  },
+  {
+    "revision": "c196ecaa446148f255db1dc8a122ce07",
+    "url": "/credera-forcast/static/media/Clear.c196ecaa.svg"
+  },
+  {
+    "revision": "ce713cdb4a9a70e9f264c5918a4e130f",
+    "url": "/credera-forcast/static/media/Cloudy.ce713cdb.svg"
+  },
+  {
+    "revision": "8fcbef549788f12c60cd4177befac13d",
+    "url": "/credera-forcast/static/media/Dallas.8fcbef54.png"
+  },
+  {
+    "revision": "0fc41951f9720be2de100df886231c7a",
+    "url": "/credera-forcast/static/media/Drizzle.0fc41951.svg"
+  },
+  {
+    "revision": "2d2b9ef707f0adf987f2690ce93bc626",
+    "url": "/credera-forcast/static/media/Haze.2d2b9ef7.svg"
+  },
+  {
+    "revision": "fcac44344aaa4abf7afef245cbf25f2b",
+    "url": "/credera-forcast/static/media/Partly-Cloudy.fcac4434.svg"
+  },
+  {
+    "revision": "525348cf8b0bbd5bf2389bdd12e9a4a2",
+    "url": "/credera-forcast/static/media/Rain.525348cf.svg"
+  },
+  {
+    "revision": "153ee175556321b57738a649f253c91a",
+    "url": "/credera-forcast/static/media/Snow.153ee175.svg"
+  },
+  {
+    "revision": "d5bd245ec233887d14bd811721a285ea",
+    "url": "/credera-forcast/static/media/Thunderstorm.d5bd245e.svg"
+  },
+  {
+    "revision": "02c6dcf03185fb02719109cc092ad73d",
+    "url": "/credera-forcast/static/media/location_on - material.02c6dcf0.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/credera-forcast/static/media/logo.5d5d9eef.svg"
+  }
+]);
